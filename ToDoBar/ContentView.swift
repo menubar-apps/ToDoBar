095fee494572d4b1010c3e360bcfe1cc26e06869
streetturtle <streetturtle@gmail.com>
2023-11-13 21:14:18 -0500

    Allow editing items

--- ToDoBar/ContentView.swift
+++ ToDoBar/ContentView.swift
@@ -9,19 +9,23 @@ import SwiftUI
 
 import SwiftUI
 import Defaults
-import LaunchAtLogin
+//import LaunchAtLogin
 
 struct ContentView: View {
     
     @Default(.todos) var todos
-
+    
     @NSApplicationDelegateAdaptor private var appDelegate: AppDelegate
     
     @State private var text: String = ""
+    @State private var editedItem: String = ""
     @FocusState private var isTextFieldFocused: Bool
-
+    
     @State private var newTodo: String = ""
-    @ObservedObject private var launchAtLogin = LaunchAtLogin.observable
+    //    @ObservedObject private var launchAtLogin = LaunchAtLogin.observable
+    
+    @State private var editedItemIdx: Int = -1
+    @FocusState private var focusedField: Bool
 
     var body: some View {
         VStack {
@@ -37,12 +41,35 @@ struct ContentView: View {
                             .padding(.top, 1)
                     }.buttonStyle(PlainButtonStyle())
                     
-                    Text(todos[index].text)
-                        .foregroundColor(todos[index].isDone ? .secondary : .primary)
-                        .strikethrough(todos[index].isDone)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                    if editedItemIdx == index {
+                        TextField("", text: $editedItem)
+                            .focused($focusedField)
+                    } else {
+                        Text(todos[index].text)
+                            .foregroundColor(todos[index].isDone ? .secondary : .primary)
+                            .strikethrough(todos[index].isDone)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
                     
                     Spacer()
+                    
+                    Button(action: {
+                        if editedItemIdx == index {
+                            editedItemIdx = -1
+                            todos[index] = Todo(text: editedItem, isDone: todos[index].isDone)
+                            focusedField = false
+                        } else {
+                            editedItemIdx = index
+                            editedItem = todos[index].text
+                            focusedField = true
+                        }
+                    }) {
+                        Image(systemName: editedItemIdx == index ? "square.and.arrow.down" : "pencil.circle")
+                            .resizable()
+                            .frame(width: 18, height: 18)
+                            .padding(.top, 1)
+                            .foregroundColor(.secondary)
+                    }.buttonStyle(PlainButtonStyle())
                     Button(action: {
                         todos.remove(at: index)
                     }) {
@@ -54,7 +81,7 @@ struct ContentView: View {
                     }.buttonStyle(PlainButtonStyle())
                 }
             }
-
+            
             HStack {
                 
                 TextField("Type a task and hit enter", text: $newTodo)
@@ -77,9 +104,9 @@ struct ContentView: View {
                         newTodo = ""
                     }
                     .onAppear {
-                                isTextFieldFocused = true
-                            }
-
+                        isTextFieldFocused = true
+                    }
+                
                 Spacer()
                 Menu {
                     Button(action: {
@@ -93,7 +120,7 @@ struct ContentView: View {
                         Label("Clear All", systemImage: "book")
                     }
                     Divider()
-                    Toggle("Launch at login", isOn: $launchAtLogin.isEnabled)
+                    //                    Toggle("Launch at login", isOn: $launchAtLogin.isEnabled)
                     Divider()
                     Button(action: { appDelegate.openAboutWindow(nil) } ) {
                         Label("About ToDoBar", systemImage: "books.vertical")
@@ -102,12 +129,12 @@ struct ContentView: View {
                         Label("Quit", systemImage: "books.vertical")
                     }
                 } label: {}
-                .menuStyle(BorderlessButtonMenuStyle())
-                .frame(width: 14, height: 16)
-                .padding(8)
-                .padding(.trailing, 2)
-                .background(Color.accentColor)
-                .cornerRadius(8)
+                    .menuStyle(BorderlessButtonMenuStyle())
+                    .frame(width: 14, height: 16)
+                    .padding(8)
+                    .padding(.trailing, 2)
+                    .background(Color.accentColor)
+                    .cornerRadius(8)
             }
         }.padding(8)
     }
